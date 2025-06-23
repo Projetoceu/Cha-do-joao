da88ffe50df272a1e20da8d7dcdf5dc960eef613
Projetoceu <renatoaugustod@gmail.com>
2025-06-23 16:17:54 -0300

    Create listar-produtos.js

---
+++ listar-produtos.js
@@ -0,0 +1,15 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+exports.handler = async () => {
+  const { data, error } = await supabase.from('produtos').select('*');
+  if (error) return { statusCode: 500, body: JSON.stringify(error) };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+};
